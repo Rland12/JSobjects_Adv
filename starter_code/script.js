a9f71de0506acb028552191226e0725222ad8d1a
Rland12 <rland7596@withs.org>
2017-01-04 23:30:14 +0000

    JS Objects

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,19 +1,22 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-
+console.log(library.music[0].albums[0].year);
 //Task 2: Console the name of the song Lady Gaga released in 2006 
-
+console.log(library.music[5].albums[0].song);
 
 $( document ).ready(function() {
     //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-
+$("#songs").html(library.music[6].albums[0].song);
     
     //Task 4: (project) Create a site that can display all songs from an artist specified by a user search. (This will require an input field for the user to type into.. OR a drop down menu populated with Artist names)
     
         //Step 4.1 - Loop through all the music objects and console.log the artists
-        
+        for(var i = 0; i< library.music.length; i++){
+            
+            console.log(library.music[i].artist);
+        }
         //Step 4.2 - fill in the "#artists" select box with each artist name as an option
         //      Examples: https://gist.github.com/paulund/5861214
-        
+        $("#artists").append($("<option></option>").attr("value").text());
         //Step 4.3 - Fill in the ID of the form field to handle the submission 
         $( "#songsearch" ).submit(function( event ) {
           event.preventDefault();
